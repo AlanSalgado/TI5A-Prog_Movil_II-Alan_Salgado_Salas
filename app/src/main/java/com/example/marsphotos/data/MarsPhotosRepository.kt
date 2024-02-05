2ae9fecf546fbf481ad243666ceef7235fe5cfe7
CristianATZ <s20120154@alumnos.itsur.edu.mx>
2024-02-05 15:04:53 -0600

    Insercion de dependencias

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,13 +1,13 @@
 package com.example.marsphotos.data
 import com.example.marsphotos.model.MarsPhoto
-import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApiService
 
 interface MarsPhotosRepository {
     suspend fun gerMarsPhotos(): List<MarsPhoto>
 }
 
-class NetworkMarsPhotosRepository(): MarsPhotosRepository {
-    override suspend fun gerMarsPhotos(): List<MarsPhoto> {
-        return MarsApi.retrofitService.getPhotos()
-    }
+class NetworkMarsPhotosRepository(
+    private val marsApiService: MarsApiService
+): MarsPhotosRepository {
+    override suspend fun gerMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
 }
