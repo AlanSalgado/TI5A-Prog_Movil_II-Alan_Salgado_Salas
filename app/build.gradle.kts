9f13d8cb420fe6b32eb03e89c0e650524a260d0a
CristianATZ <s20120154@alumnos.itsur.edu.mx>
2024-02-25 19:49:37 -0600

    Pruebas para las implementacinoes de Worker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.test:core-ktx:1.5.0")
+    implementation("androidx.work:work-testing:2.9.0")
+    implementation("junit:junit:4.12")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+
 }
